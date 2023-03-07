ae32f2a75683d34cbb0f139f5c97147b30297588
talek <talek@LAPTOP-PV2O8EDM>
2023-03-07 23:41:06 +0530

    Add project files.

---
+++ Migrations/202303061849407_prod_app.cs
@@ -0,0 +1,28 @@
+﻿namespace Hrutik.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class prod_app : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductID = c.Int(nullable: false, identity: true),
+                        ProductName = c.String(nullable: false),
+                        CategoryID = c.Int(nullable: false),
+                        CategoryName = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Hrutik.Models
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        [DisplayName("ProductID")]
+        public int ProductID { get; set; }
+
+        [Required]
+        [DisplayName("ProductName")]
+        public string ProductName { get; set; }
+
+        [Required]
+        [DisplayName("CategoryID")]
+        public int CategoryID { get; set; }
+
+        [Required]
+        [DisplayName("CategoryName")]
+        public string CategoryName { get; set; }
+    }
+}
